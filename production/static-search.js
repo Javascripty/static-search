60d6e49ea346c9aa45be0390daf608b7eba6ccc4
javascripty <ed2711uribe@gmail.com>
2022-08-25 17:13:39 -0700

    Rebuilt production directory.

--- production/static-search.js
+++ production/static-search.js
@@ -12,41 +12,39 @@ class StaticSearch {
     #input;
     #searchableData;
 
-    // Default search settings
+    // Default static-search settings
     #defaults = {
         pathToTheSearchableData: undefined,
         searchFormId: "#static-search-form",
         searchFormInputId: "#static-search-query",
         searchResultsTemplate: undefined,
-        noSearchResultsFoundTemplate: `<p class="no-results-found">...Sorry no search results found</p>`,
+        noSearchResultsFoundMessage: "Sorry no search results found",
         showStaticSearchLogo: true,
         highlightKeywords: true,
     };
 
-    // Define instance properties
+    // Define: instance properties
     constructor(options = {}) {
         this.#settings = Object.assign({}, this.#defaults, options);
         this.#input = document.querySelector(`${this.#settings.searchFormInputId}`);
         this.#form = document.querySelector(`${this.#settings.searchFormId}`);
 
         // Event listeners
-        /**
-         * On search form "mouseover" event - fetch the searchable json data
-         * Note: this event will fire only once.
-         */
+
+        // Fetch searchable data
         this.#form.addEventListener("mouseover", async (event) => {
 
-            // Check if the searchable json data is available in sessionStorage
+            // Check if the searchable data is available in sessionStorage
             if (sessionStorage.getItem("searchable-json-data")) {
 
-                // Set the searachable json data
+                // Set the searachable data
                 this.#searchableData = JSON.parse(sessionStorage.getItem("searchable-json-data"));
             }
             else {
 
-                // If not then send a fetch request for the searchable json data, and store it into sessionStorage
+                // If not: send a fetch request for the searchable data, and also store in sessionStorage
                 try {
-                    // Fetch the searchable json data
+                    // Fetch the searchable data
                     let response = await fetch(this.#settings.pathToTheSearchableData);
 
                     // If the fetch failed, throw an error
@@ -54,10 +52,10 @@ class StaticSearch {
                         throw "Sorry couldn't fetch the data";
                     }
 
-                    // Set the searchable data to a private instance field
+                    // Set the searchable data to a private instance field.
                     this.#searchableData = await response.json();
 
-                    // Save the searchable json data to sessionStorage
+                    // Save the searchable json data to sessionStorage.
                     sessionStorage.setItem("searchable-json-data", JSON.stringify(this.#searchableData));
                 }
                 catch (error) {
@@ -67,13 +65,13 @@ class StaticSearch {
 
         }, { once: true });
 
-        // On search form "submit" event
+        // Render search matches.
         this.#form.addEventListener("submit", (event) => {
 
             // Prevent the default "submit" behaviour.
             event.preventDefault();
 
-            // Process to remove any previously rendered <.static-search-result> components.
+            // Remove any previously rendered <static-search-results> components.
             let component = document.querySelector("#static-search-results");
 
             if (component != null) {
@@ -81,19 +79,14 @@ class StaticSearch {
             }
 
             
-            // Process to search for matches:
-
-            // Create a deep-clone of the searchable data.
+            // Search for matches.
+            // "Deep-clone" of the searchable data.
             let searchableDataClone = structuredClone(this.#searchableData);
 
-            // An array of objects whose properties match the search query string
+            // Array of objects whose properties match the search query string.
             let matches = searchableDataClone.filter((object) => {
                     for (let property in object) {
-
-                        if (this.#stringMatching({ 
-                            word: `${this.#input.value}`, 
-                            string: object[property] 
-                        })) {
+                        if (this.#stringMatching( { word: `${this.#input.value}`, string: object[property] } )) {
                             return true;
                         }
                     }
@@ -101,8 +94,8 @@ class StaticSearch {
                     return false;
             });
 
+            // Highlight the keywords inside the matching objects.
             if (this.#settings.highlightKeywords) {
-                // Highlight the keywords inside the matching objects.
                 matches.forEach( (object) => {
                     for (let property in object) {
                         object[property] = this.#highlight({ string: object[property], keyword: `${this.#input.value}` });
@@ -112,30 +105,39 @@ class StaticSearch {
                 });
             }
 
-            // Process to render the search results:
-
-            // The component that will hold the returned search results
+            // Render the search matches.
+            // The component that will hold the returned search matches.
             let results = document.createElement("ul");
             results.setAttribute('id', 'static-search-results');
 
-            // If there are no search matches found:
+            // Render the static-search logo.
+            if (this.#settings.showStaticSearchLogo) {
+                
+                let logo = document.createElement('li');
+                logo.setAttribute('id', 'static-search-logo');
+                logo.innerHTML = `<p>Search provided by <a href="#">static search</a></p>`;
+
+                // Insert the logo after the last child of the results element.
+                results.append(logo);
+            }
+
             if (matches.length <= 0) {
 
-                // The element that will hold the no results found message
+                // Render the no search matches found message.
                 let noresults = document.createElement("li");
+                let message = document.createElement("p");
 
-                // Insert the no results message
-                noresults.innerHTML = this.#settings.noSearchResultsFoundTemplate;
-
+                message.setAttribute("id", "no-results-found");
+                message.innerText = this.#settings.noSearchResultsFoundMessage;
+                noresults.append(message);
                 results.prepend(noresults);
 
                 // Insert the "no results found" message after the last child of the #form element.
                 return this.#form.append(results);
+
             } else {
 
-                // Process to render the search matches found:
-
-                // Content for the search results found.
+                // Render the search matches found.
                 let content = matches.map((match) => {
 
                     let li = document.createElement("li");
@@ -148,31 +150,19 @@ class StaticSearch {
 
                 results.innerHTML = content;
 
-                if (this.#settings.showStaticSearchLogo) {
-
-                    // Insert the static search logo.
-                    let logo = document.createElement('li');
-                    logo.setAttribute('id', 'static-search-logo');
-                    logo.innerHTML = `<p>Search provided by <a href="#">static search</a></p>`;
-
-                    // Insert the logo after the last child of the results element.
-                    results.append(logo);
-                }
-
-                // Insert the search results component.
                 this.#form.append(results);
             }
         });
 
-        // Close the matching search results ui component when the person clicks out of the search form
+        // Remove search results.
         document.addEventListener("click", (event) => {
             if (event.target !== this.#form) {
 
-                // remove any previously rendered <#result> components.
-                let child = document.querySelector(".static-search-result");
+                // Remove any previously rendered search results.
+                let results = document.querySelector("#static-search-results");
 
-                if (child != null) {
-                    this.#form.removeChild(child);
+                if (results != null) {
+                    this.#form.removeChild(results);
                 }
             }
         });
